de44f5958bc2509b20c10326f1f0e48dfb3c9997
Odilah <odilahrandrianjanahary@hotmail.com>
2025-04-01 05:09:41 +0300

    hjkzrbg
    bfhgefj

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -1,7 +1,15 @@
+import os
+
+from djangoProject.settings import BASE_DIR
 
 # ...existing code...
 
 # Tell Django to use your custom User model
 AUTH_USER_MODEL = 'utilisateurs.User'
-
-# ...existing code...
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
--- djangoProject/utilisateurs/urls.py
+++ djangoProject/utilisateurs/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('users/', views.user_list_view, name='user_list'),
+    path('conversations/<int:user_id>/', views.get_conversation, name='get_conversation'),
+    path('send-message/', views.send_message, name='send_message'),
+path('conversations/<int:conversation_id>/new-messages/<str:last_message_time>/',
+         views.get_new_messages,
+         name='get_new_messages'),
 ]
